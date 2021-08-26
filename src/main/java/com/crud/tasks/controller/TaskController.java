ee28f72a6c2bd6b670e775d7d1bd04ef3db9bed8
Dawid Kocik <kocik.dawid@wp.pl>
2021-08-26 17:06:22 +0200

    Module 19.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -7,7 +7,6 @@ import com.crud.tasks.service.DbService;
 import lombok.RequiredArgsConstructor;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
