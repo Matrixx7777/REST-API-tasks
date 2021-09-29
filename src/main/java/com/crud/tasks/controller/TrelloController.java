0da0880e29d91e447c0e0748db969d70c058b1b1
Dawid Kocik <kocik.dawid@wp.pl>
2021-09-29 18:04:42 +0200

    Module 22.4

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -30,7 +30,6 @@ public class TrelloController {
 //                        trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()
 //                );
 //            });
-//
 //        });
 //    }
 
